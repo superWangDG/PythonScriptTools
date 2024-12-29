6e33337bd0ca2b2320df4131d184934aecb62cb6
superwangdg <superwangdg@163.com>
2024-12-29 23:10:20 +0800

    添加执行 ffmpeg 下载视频

--- main_application.py
+++ main_application.py
@@ -2,6 +2,7 @@ from scene.ios_podfile_handle import run_podfile_handle
 from scene.upload_bugly import run_update_bugly
 from scene.language_to_localizable import run_exc_lang_to_localizable_files
 from scene.excel_orgifile_match_replace import run_exc_org_match_rep
+from scene.ffmpeg_donwload_files import run_download_medias
 
 if __name__ == "__main__":
     # 执行功能分选
@@ -9,7 +10,8 @@ if __name__ == "__main__":
         "1.Bugly 符号表上传",
         "2.excel 文件开发多语言生成",
         "3.excel 指定的语言文本匹配替换",
-        "4.执行iOS项目Cocoapods 的管理操作"
+        "4.执行iOS项目Cocoapods 的管理操作",
+        "5.使用ffmpeg 下载媒体"
     ]
     print("请选择一个功能:")
     for item in funcs:
@@ -25,6 +27,8 @@ if __name__ == "__main__":
             run_exc_org_match_rep()
         elif choice == 4:
             run_podfile_handle()
+        elif choice == 5:
+            run_download_medias()
         else:
             print(f"无效的选择，请输入 1 到 {len(funcs)} 之间的数字。")
     except ValueError:
